bb71d29e7878b4fa60c9b36500bb60278761eef2
face-do <mba@MacBook-Air-2.local>
2012-09-27 21:58:28 +0900

    first commit

---
+++ lib/motion-rails-model.rb
@@ -0,0 +1,9 @@
+require "motion-rails-model/version"
+
+module Motion
+  module Rails
+    module Model
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/motion-rails-model/version.rb
@@ -0,0 +1,7 @@
+module Motion
+  module Rails
+    module Model
+      VERSION = "0.0.1"
+    end
+  end
+end
